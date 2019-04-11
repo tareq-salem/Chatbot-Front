33415e275559eb4ece4aa247ad4fd33d4de93f91
Takeshi69 <qdudon@gmail.com>
2019-04-11 09:29:19 +0200

    starter-kit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import ChatBot from 'react-simple-chatbot';
+import { ThemeProvider } from 'styled-components';
+
+import steps from './config/steps';
+
 import './App.css';
+import logo from './assets/images/Chatbot-logo.png';
+import logo_stayregular from './assets/images/stayregular-logo-black.png';
+import logo_kushyapi from './assets/images/kushyapi-logo-black.png';
+import logo_chatbot from './assets/images/react-simple-chatbot.svg';
+import icon_user from './assets/images/icon-user.png';
+import './assets/css/main.css';
+import './assets/css/bootstrap-grid.min.css';
+
+import chat_styles from './config/chatstyles'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header className="Header">
+          <img src={logo} className="Header-logo" alt="logo" />
         </header>
+        <div className="container content">
+          <div className="row justify-content-md-center">
+            <div className="col">
+              <ThemeProvider theme={chat_styles}>
+                <ChatBot
+                  steps={steps}
+                  headerTitle="HOC - Chatbot"
+                  placeholder="Type away..."
+                  customDelay="500"
+                  botAvatar={logo_chatbot}
+                  userAvatar={icon_user}
+                />
+              </ThemeProvider>
+            </div>
+          </div>
+        </div>
+        <footer className="Footer">
+          <p className="credit">Shoutout to <a href="https://lucasbassetti.com.br/react-simple-chatbot/" className="chatbot"><img src={logo_chatbot} alt="React Simple Chatbot" /></a>.</p>
+        </footer>
       </div>
     );
   }
